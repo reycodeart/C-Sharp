f58c5ae148f9fd8165c231c4e1b1f5e85d0ee768
Reyhan <75200483+reycodeart@users.noreply.github.com>
2021-09-02 17:15:14 +0300

    Add files via upload

---
+++ Ders1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YouCanDoIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
